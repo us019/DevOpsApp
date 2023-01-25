4c3f5c808973aeef2cb7f622bf102e5e95879fc7
imran <imran@gmail.com>
2023-01-25 10:57:27 +0530

    Added java demo application files

---
+++ src/main/java/example/devOps/devops/Main.java
@@ -0,0 +1,12 @@
+package example.devOps.devops;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(checkIfInputIsAnEvenNumber(10));
+
+	}
+public static boolean checkIfInputIsAnEvenNumber(int number) {
+	return number%2==0;
+}
+}
---
+++ src/test/java/example/devOps/devops/TestMain.java
@@ -0,0 +1,14 @@
+package example.devOps.devops;
+
+
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Test;
+public class TestMain {
+
+	@Test
+public void TestInputIsEven() {
+	assertTrue(Main.checkIfInputIsAnEvenNumber(0));
+}
+}
